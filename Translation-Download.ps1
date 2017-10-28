17e37a2af1bb55943534505116ee193664d781f7
Bastian Eicher <bastian@eicher.net>
2017-10-28 17:20:47 +0200

    Added scripts for downloading/uploading translations

---
+++ Translation-Download.ps1
@@ -0,0 +1,14 @@
+Param ([Parameter(Mandatory=$True)][String]$User, [Parameter(Mandatory=$True)][String]$Password)
+$ErrorActionPreference = "Stop"
+$ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+function get ($relativeUri, $filePath) {
+    0install run http://repo.roscidus.com/utils/curl -k -L --user "${User}:${Password}" -o $filePath https://www.transifex.com/api/2/project/0install-win/$relativeUri
+}
+
+function download($slug, $pathBase) {
+    get "resource/$slug/translation/el/?file" "$pathBase.el.resx"
+    get "resource/$slug/translation/tr/?file" "$pathBase.tr.resx"
+}
+
+download publish "$ScriptDir\src\Publish\Properties\Resources"
