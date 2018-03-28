b6353db2029032aea87865da1d05011f3a96bb8c
Bastian Eicher <bastian@eicher.net>
2018-03-29 01:22:04 +0200

    Tweaked build

--- release/build.ps1
+++ release/build.ps1
@@ -1,17 +1,16 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+# Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    # Put 0install in PATH using Bootstrapper
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/zero-install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
+    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
 }
 
 rm -Force ..\build\Release\*.xml
 rm -Force ..\build\Release\*.pdb
 
-if ($Host.Name -ne 'ConsoleHost') {$ErrorActionPreference = "ContinueSilent"} # Avoid treating stderr output as failure condition
-0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$(Get-Content ..\VERSION)
+cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$(Get-Content ..\VERSION) 2>&1" # Redirect stderr to stdout
 
 popd
