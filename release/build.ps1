e93cd6c95686ed1ee9a329d10c2e9a75731d332b
Bastian Eicher <bastian@eicher.net>
2018-06-14 23:27:21 +0200

    Improved building releases on AppVeyor

--- release/build.ps1
+++ release/build.ps1
@@ -1,7 +1,9 @@
+﻿Param ([switch]$GitHubRelease)
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 $Version = Get-Content ..\VERSION
+if ($GitHubRelease -ne "stable") {$Version += "-pre"}
 
 # Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
@@ -10,18 +12,16 @@ if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
 }
 
-# Do not include .NET XML documentation in release
-rm -Force ..\build\Release\*.xml
-
-# Do not include debug symbols in release
-rm -Force ..\build\Release\*.pdb
+# Exclude .NET XML Documentation and Debug Symbols from release
+rm -Force ..\build\Release\*.xml,..\build\Release\*.pdb
 
 # Build feed and archive
-cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$Version 2>&1" # Redirect stderr to stdout
+$stability = if($Version.Contains('-')) {'developer'} else {'stable'}
+cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$Version stability=$stability 2>&1" # Redirect stderr to stdout
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
-# Patch URL to point to GitHub release for tagged builds
-if ($env:APPVEYOR_REPO_TAG -eq "true") {
+# Patch archive URL to point to GitHub Release
+if ($GitHubRelease) {
     $path = Resolve-Path "ZeroInstall_Tools-$Version.xml"
     [xml]$xml = Get-Content $path
     $xml.interface.group.implementation.archive.href = "https://github.com/0install/0publish-win/releases/download/$Version/$($xml.interface.group.implementation.archive.href)"
