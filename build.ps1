de09d462f9b37a198fc031c619f96e378106e3d7
Bastian Eicher <bastian@eicher.net>
2018-09-28 16:53:41 +0200

    Renamed release/ to feed/

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@ $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 src\build.ps1 $Version
-release\build.ps1 $Version
+feed\build.ps1 $Version
 
 popd
--- feed/build.ps1
+++ feed/build.ps1
@@ -1,4 +1,4 @@
-﻿Param ($Version = "0.1.0-pre", [switch]$GitHubRelease)
+﻿Param ($Version = "0.1.0-pre")
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
@@ -16,13 +16,5 @@ rm -Force ..\artifacts\Release\*.xml,..\artifacts\Release\*.pdb
 $stability = if($Version.Contains('-')) {'developer'} else {'stable'}
 cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$Version stability=$stability 2>&1" # Redirect stderr to stdout
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
-
-# Patch archive URL to point to GitHub Release
-if ($GitHubRelease) {
-    $path = Resolve-Path "ZeroInstall_Tools-$Version.xml"
-    [xml]$xml = Get-Content $path
-    $xml.interface.group.implementation.archive.href = "https://github.com/0install/0publish-win/releases/download/$Version/$($xml.interface.group.implementation.archive.href)"
-    $xml.Save($path)
-}
 
 popd
