e74a0656c9cc4e63bf13a87a2380827b7bb23735
Bastian Eicher <bastian@eicher.net>
2019-03-02 17:54:39 +0100

    Improved high DPI scaling

--- src/Publish.WinForms/MainForm.Designer.cs
+++ src/Publish.WinForms/MainForm.Designer.cs
@@ -315,6 +315,8 @@ namespace ZeroInstall.Publish.WinForms
             // MainForm
             // 
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 551);
             this.Controls.Add(this.feedStructureEditor);
             this.Controls.Add(this.toolStrip);
