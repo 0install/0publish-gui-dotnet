2e8828260fa599dc8f757279e4485f3e48bcf100
Bastian Eicher <bastian@eicher.net>
2019-05-07 23:48:10 +0200

    Report build failures more reliably

--- src/build.ps1
+++ src/build.ps1
@@ -18,5 +18,6 @@ SearchAndReplace $AssemblyVersion GlobalAssemblyInfo.cs -PatternLeft 'AssemblyVe
 $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
 $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 . $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
