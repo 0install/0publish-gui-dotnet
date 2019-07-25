591028712ef12ad0013c91bcfc62c9e4b623e62e
Bastian Eicher <bastian@eicher.net>
2019-07-25 19:00:37 +0200

    Improved feed template

--- feed/build.ps1
+++ feed/build.ps1
@@ -1,4 +1,4 @@
-﻿Param ($Version = "1.0.0-pre")
+﻿Param ($Version = "1.0.0-pre", [switch]$GitHubReleas)
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
@@ -17,5 +17,13 @@ rm -Force ..\artifacts\Release\*.xml,..\artifacts\Release\*.pdb
 $stability = if($Version.Contains('-')) {'developer'} else {'stable'}
 cmd /c "0install run --batch http://0install.net/tools/0template.xml ZeroInstall_Tools.xml.template version=$Version stability=$stability 2>&1" # Redirect stderr to stdout
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+
+# Patch archive URL to point to GitHub Release
+if ($GitHubRelease) {
+    $path = Resolve-Path "ZeroInstall_Tools-$Version.xml"
+    [xml]$xml = Get-Content $path
+    $xml.interface.group.implementation.archive.href = "https://github.com/0install/0publish-win/releases/download/$Version/$($xml.interface.group.implementation.archive.href)"
+    $xml.Save($path)
+}
 
 popd
