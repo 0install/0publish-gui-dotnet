4e3239230117474350a6192104bd2782b8d42f04
Bastian Eicher <bastian@eicher.net>
2020-02-03 18:46:57 +0100

    Support VS Build Tools in build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -15,7 +15,7 @@ $AssemblyVersion = $Version.Split("-")[0]
 SearchAndReplace $AssemblyVersion GlobalAssemblyInfo.cs -PatternLeft 'AssemblyVersion\("' -PatternRight '"\)'
 
 # Compile source code
-$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
+$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -latest -property installationPath -format value
 $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 . $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
