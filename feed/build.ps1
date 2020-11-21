eed7697afcdad77175b93714b1e5968e023ea4f5
Bastian Eicher <bastian@eicher.net>
2020-11-21 14:10:03 +0100

    Use latest released Bootstrapper to run 0template

--- feed/build.ps1
+++ feed/build.ps1
@@ -2,17 +2,6 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-# Ensure 0install is in PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    $downloadDir = "$env:LOCALAPPDATA\0install.net\bootstrapper"
-    if (!(Test-Path "$downloadDir\0install.exe")) {
-        mkdir -Force $downloadDir | Out-Null
-        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls11,Tls12'
-        Invoke-WebRequest "https://get.0install.net/0install.exe" -OutFile "$downloadDir\0install.exe"
-    }
-    $env:PATH = "$env:PATH;$downloadDir"
-}
-
 # Exclude .NET XML Documentation and Debug Symbols from release
 rm -Force ..\artifacts\Release\*.xml,..\artifacts\Release\*.pdb
 
@@ -20,7 +9,7 @@ rm -Force ..\artifacts\Release\*.xml,..\artifacts\Release\*.pdb
 $stability = if($Version.Contains("-")) {"developer"} else {"stable"}
 
 # Build feed and archive
-cmd /c "0install run --batch http://0install.net/tools/0template.xml 0publish-win.xml.template version=$Version stability=$stability 2>&1" # Redirect stderr to stdout
+..\0install.ps1 run --batch http://0install.net/tools/0template.xml 0publish-win.xml.template version=$Version stability=$stability
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 # Patch archive URL for release builds
