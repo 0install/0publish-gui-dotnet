61a7baf5da07f8a4b0b547421e47fc49174311c6
Bastian Eicher <bastian@eicher.net>
2022-03-29 10:23:28 +0200

    Use top-level entry point

--- src/Publish.Cli/Program.cs
+++ src/Publish.Cli/Program.cs
@@ -2,91 +2,78 @@
 // Licensed under the GNU Lesser Public License
 
 using NanoByte.Common.Net;
+using ZeroInstall.Publish.Cli;
 using ZeroInstall.Store.Implementations;
 using ZeroInstall.Store.Trust;
 
-namespace ZeroInstall.Publish.Cli;
+ProcessUtils.SanitizeEnvironmentVariables();
+NetUtils.ApplyProxy();
 
-/// <summary>
-/// Launches a command-line tool for editing Zero Install feed XMLs.
-/// </summary>
-public static class Program
+// Automatically show help for missing args
+if (args.Length == 0) args = new[] {"--help"};
+
+try
 {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    private static int Main(string[] args)
-    {
-        ProcessUtils.SanitizeEnvironmentVariables();
-        NetUtils.ApplyProxy();
-
-        // Automatically show help for missing args
-        if (args.Length == 0) args = new[] {"--help"};
-
-        try
-        {
-            using var handler = new AnsiCliTaskHandler();
-            var command = (args.FirstOrDefault() == "capture")
-                ? (ICommand)new CaptureCommand(args.Skip(1), handler)
-                : new PublishCommand(args, handler);
-            return (int)command.Execute();
-        }
-        #region Error hanlding
-        catch (OperationCanceledException)
-        {
-            return (int)ExitCode.UserCanceled;
-        }
-        catch (ArgumentException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.InvalidArguments;
-        }
-        catch (OptionException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.InvalidArguments;
-        }
-        catch (WebException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.WebError;
-        }
-        catch (InvalidDataException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.InvalidData;
-        }
-        catch (IOException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.IOError;
-        }
-        catch (UnauthorizedAccessException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.AccessDenied;
-        }
-        catch (DigestMismatchException ex)
-        {
-            Log.Info(ex.LongMessage);
-            Log.Error(ex);
-            return (int)ExitCode.DigestMismatch;
-        }
-        catch (KeyNotFoundException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.InvalidArguments;
-        }
-        catch (WrongPassphraseException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.InvalidArguments;
-        }
-        catch (NotSupportedException ex)
-        {
-            Log.Error(ex);
-            return (int)ExitCode.NotSupported;
+    using var handler = new AnsiCliTaskHandler();
+    var command = (args.FirstOrDefault() == "capture")
+        ? (ICommand)new CaptureCommand(args.Skip(1), handler)
+        : new PublishCommand(args, handler);
+    return (int)command.Execute();
+}
+#region Error hanlding
+catch (OperationCanceledException)
+{
+    return (int)ExitCode.UserCanceled;
+}
+catch (ArgumentException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.InvalidArguments;
+}
+catch (OptionException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.InvalidArguments;
+}
+catch (WebException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.WebError;
+}
+catch (InvalidDataException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.InvalidData;
+}
+catch (IOException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.IOError;
+}
+catch (UnauthorizedAccessException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.AccessDenied;
+}
+catch (DigestMismatchException ex)
+{
+    Log.Info(ex.LongMessage);
+    Log.Error(ex);
+    return (int)ExitCode.DigestMismatch;
+}
+catch (KeyNotFoundException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.InvalidArguments;
+}
+catch (WrongPassphraseException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.InvalidArguments;
+}
+catch (NotSupportedException ex)
+{
+    Log.Error(ex);
+    return (int)ExitCode.NotSupported;
         }
         #endregion
-    }
-}
