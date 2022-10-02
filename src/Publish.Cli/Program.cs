f5cf48dd0fa5a740e19e7f16cea8ac204989297e
Bastian Eicher <bastian@eicher.net>
2022-10-02 14:58:43 +0200

    Refactored Program.cs

--- src/Publish.Cli/Program.cs
+++ src/Publish.Cli/Program.cs
@@ -9,14 +9,15 @@ using ZeroInstall.Store.Trust;
 ProcessUtils.SanitizeEnvironmentVariables();
 NetUtils.ApplyProxy();
 
-// Automatically show help for missing args
-if (args.Length == 0) args = new[] {"--help"};
+using var handler = new AnsiCliTaskHandler();
 
 try
 {
-    using var handler = new AnsiCliTaskHandler();
-    var command = (args.FirstOrDefault() == "capture")
-        ? (ICommand)new CaptureCommand(args.Skip(1), handler)
+    // Automatically show help for missing args
+    if (args.Length == 0) args = new[] {"--help"};
+
+    ICommand command = args.FirstOrDefault() == "capture"
+        ? new CaptureCommand(args.Skip(1), handler)
         : new PublishCommand(args, handler);
     return (int)command.Execute();
 }
@@ -25,55 +26,40 @@ catch (OperationCanceledException)
 {
     return (int)ExitCode.UserCanceled;
 }
-catch (ArgumentException ex)
+catch (Exception ex) when (ex is ArgumentException or OptionException or KeyNotFoundException or WrongPassphraseException)
 {
-    Log.Error(ex);
-    return (int)ExitCode.InvalidArguments;
-}
-catch (OptionException ex)
-{
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.InvalidArguments;
 }
 catch (WebException ex)
 {
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.WebError;
 }
 catch (InvalidDataException ex)
 {
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.InvalidData;
 }
 catch (IOException ex)
 {
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.IOError;
 }
 catch (UnauthorizedAccessException ex)
 {
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.AccessDenied;
 }
 catch (DigestMismatchException ex)
 {
     Log.Info(ex.LongMessage);
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.DigestMismatch;
-}
-catch (KeyNotFoundException ex)
-{
-    Log.Error(ex);
-    return (int)ExitCode.InvalidArguments;
-}
-catch (WrongPassphraseException ex)
-{
-    Log.Error(ex);
-    return (int)ExitCode.InvalidArguments;
 }
 catch (NotSupportedException ex)
 {
-    Log.Error(ex);
+    handler.Error(ex);
     return (int)ExitCode.NotSupported;
 }
 #endregion
